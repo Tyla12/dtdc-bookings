5693f5f551771467a21b17a4a0cbf08e69fa5365
Tyla12 <147553496+Tyla12@users.noreply.github.com>
2025-11-27 21:50:08 +0200

    Update models.py

--- models.py
+++ models.py
@@ -10,7 +10,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False, index=True)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.Text)
     phone = db.Column(db.String(20), nullable=True)
     role = db.Column(db.String(20), default='official')  
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
