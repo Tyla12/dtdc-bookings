c57537d6fb01af992cde83709439fb32d9a3314e
Tyla12 <147553496+Tyla12@users.noreply.github.com>
2026-01-15 12:34:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -23,9 +23,9 @@ def create_app():
 
     # Fix Render DB URL
     if db_url.startswith("postgres://"):
-        db_url = db_url.replace("postgres://", "postgresql+psycopg2://", 1)
+        db_url = db_url.replace("postgres://", "postgresql+psycopg://", 1)
     elif db_url.startswith("postgresql://"):
-        db_url = db_url.replace("postgresql://", "postgresql+psycopg2://", 1)
+        db_url = db_url.replace("postgresql://", "postgresql+psycopg://", 1)
 
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
