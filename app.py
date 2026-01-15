0e3ff94b5d063258f670a04b57c357b77bed6f0a
Tyla12 <147553496+Tyla12@users.noreply.github.com>
2026-01-15 14:13:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, flash, request
+from rooms import seed_rooms
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_required, login_user, logout_user, current_user
 from sqlalchemy.exc import IntegrityError
@@ -35,6 +36,8 @@ def create_app():
     # Create tables
     with app.app_context():
         db.create_all()
+        seed_rooms()
+        create_demo_manager()
 
     login_manager = LoginManager()
     login_manager.login_view = 'login'
@@ -43,10 +46,6 @@ def create_app():
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
-
-    # Ensure manager exists
-    with app.app_context():
-        create_demo_manager()
 
     # --------------------- ROUTES ----------------------
 
